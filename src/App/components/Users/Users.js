c9056a0c33b44d55e88d9449e8e4068cf7be0351
lovetraindriver <lovetraindriver@gmail.com>
2022-03-22 09:19:55 +0700

    wrapped REST API requests to Users Container. Having some errors with pagination. Fix needed

--- src/App/components/Users/Users.js
+++ src/App/components/Users/Users.js
@@ -1,58 +1,32 @@
-import * as axios from 'axios';
 import React from 'react';
 import User from '../User/User';
 
-class Users extends React.Component {
-
-    componentDidMount() {
-        if (this.props.users == 0) {
-            let url = `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`
-            axios.get(url).then(
-                r=>{
-                    this.props.setUsers(r.data.items)
-                    this.props.changeTotalUsersCount(r.data.totalCount)
-                }
-            )
-        }
-    }
-
-    changingPage(i) {
-        let url = `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${i}`
-            axios.get(url).then(
-                r=>{
-                    this.props.setUsers(r.data.items)
-                }
-            )
-        this.props.changePage(i);
-    }
-        
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize),
+let Users = props => {
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize),
         pages = []
 
         for (let i=1; i <= pagesCount; i++) {
             pages.push(i)
         }
+        
+    return <div>
+        <h1>Users</h1>
+        <ul className='pagination'>
+            {
+                pages.map(i => {return <li className={ props.currentPage === i ? 'active': ''} onClick={()=> {props.changingPage(i)}}>{i}</li>})
+            }
 
-        return <div>
-            <h1>Users</h1>
-            <ul className='pagination'>
-                {
-                    pages.map(i => {return <li className={ this.props.currentPage === i ? 'active': ''} onClick={()=> {this.changingPage(i)}}>{i}</li>})
-                }
-
-            </ul>
-            <div>{
-                this.props.users.map(
-                    (user) => {
-                        if (this.props.loggedUser.id != user.id) {
-                            return <User name={user.name} photos={user.photos} id={user.id} key={user.id} status={user.status} country={user.country} city={user.city} followed={user.followed} loggedUser={this.props.loggedUser} unsubscribeUser={this.props.unsubscribeUser} subscribeUser={this.props.subscribeUser}/>
-                        }
+        </ul>
+        <div>{
+            props.users.map(
+                (user) => {
+                    if (props.loggedUser.id != user.id) {
+                        return <User name={user.name} photos={user.photos} id={user.id} key={user.id} status={user.status} country={user.country} city={user.city} followed={user.followed} loggedUser={props.loggedUser} unsubscribeUser={props.unsubscribeUser} subscribeUser={props.subscribeUser}/>
                     }
-                )
-            }</div>
-        </div>
-    }
+                }
+            )
+        }</div>
+    </div>
 }
 
 export default Users
--- src/App/components/Users/UsersContainer.js
+++ src/App/components/Users/UsersContainer.js
@@ -1,7 +1,45 @@
+import * as axios from 'axios';
 import React from 'react';
 import Users from './Users'
 import { connect } from 'react-redux';
 import { changePageAC, changeTotalUsersCountAC, setUsersAC, subscribeUserAC, unsubscribeUserAC } from '../../redux/users-reducer';
+
+class UsersAPI extends React.Component {
+
+    componentDidMount() {
+        if (this.props.users == 0) {
+            let url = `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`
+            axios.get(url).then(
+                r=>{
+                    this.props.setUsers(r.data.items)
+                    this.props.changeTotalUsersCount(r.data.totalCount)
+                }
+            )
+        }
+    }
+
+    changingPage(pageNumber) {
+        debugger
+        let url = `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`
+        axios.get(url).then(
+            r=>{
+                this.props.setUsers(r.data.items)
+            }
+        )
+        this.props.changePage(pageNumber);
+    }
+        
+    render() {return <Users changingPage = {this.changingPage}
+                     totalUsersCount = {this.props.totalUsersCount}
+                     pageSize = {this.props.pageSize}
+                     currentPage = {this.props.currentPage}
+                     users = {this.props.users}
+                     loggedUser = {this.props.loggedUser}
+                     subscribeUser = {this.props.subscribeUser}
+                     unsubscribeUser = {this.props.unsubscribeUser}
+    />
+    }
+}
 
 let mapStateToProps = (store) => {
     return {
@@ -33,6 +71,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps) (Users)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps) (UsersAPI)
 
 export default UsersContainer;
